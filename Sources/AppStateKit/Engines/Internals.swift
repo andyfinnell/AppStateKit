2a0fda48b57b32063ffe0225b42f3bc86d023d5e
Andrew Finnell <andy@losingfight.com>
2024-06-02 11:55:44 -0400

    Add escape hatch

--- Sources/AppStateKit/Engines/Internals.swift
+++ Sources/AppStateKit/Engines/Internals.swift
@@ -1,5 +1,13 @@
 
+public protocol ExposedInternal {
+    associatedtype Dependency: Dependable
+}
+
 public struct Internals {
     let dependencyScope: DependencyScope
+    
+    public subscript<E: ExposedInternal>(key: E.Type) -> E.Dependency.T {
+        dependencyScope[key.Dependency]
+    }
 }
 
