96afc9dee8c9f481b3e80187a0d0385d3c78fe11
Andrew Finnell <andy@losingfight.com>
2024-06-16 15:14:48 -0400

    MainActor annotation

--- Sources/AppStateKit/Engines/Internals.swift
+++ Sources/AppStateKit/Engines/Internals.swift
@@ -3,10 +3,10 @@ public protocol ExposedInternal {
     associatedtype Dependency: Dependable
 }
 
+@MainActor
 public struct Internals {
     let dependencyScope: DependencyScope
     
-    @MainActor
     public subscript<E: ExposedInternal>(key: E.Type) -> E.Dependency.T {
         dependencyScope[key.Dependency]
     }
